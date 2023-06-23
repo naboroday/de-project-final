21333fcb1818beba973d0b68d871356cbe99bb04
naboroday <86660091+naboroday@users.noreply.github.com>
2023-06-23 14:35:10 +0300

    Update requests.sql

--- src/sql/requests.sql
+++ src/sql/requests.sql
@@ -12,8 +12,10 @@ CREATE TABLE public.transactions_keys (
 	date_load timestamp NULL
 );
 
-
+--____________VERTICA____________ 
 -- Создание таблиц в VERTICA
+
+--transactions_1
 CREATE TABLE ST23052707__STAGING.transactions_1
 (
     operation_id varchar(60),
@@ -27,6 +29,43 @@ CREATE TABLE ST23052707__STAGING.transactions_1
     amount varchar(30)
 );
 
+CREATE PROJECTION ST23052707__STAGING.transactions_1 /*+createtype(L)*/ 
+(
+ operation_id,
+ transaction_dt,
+ account_number_from,
+ account_number_to,
+ currency_code,
+ country,
+ status,
+ transaction_type,
+ amount
+)
+AS
+ SELECT transactions_1.operation_id,
+        transactions_1.transaction_dt,
+        transactions_1.account_number_from,
+        transactions_1.account_number_to,
+        transactions_1.currency_code,
+        transactions_1.country,
+        transactions_1.status,
+        transactions_1.transaction_type,
+        transactions_1.amount
+ FROM ST23052707__STAGING.transactions_1
+ ORDER BY transactions_1.operation_id,
+          transactions_1.transaction_dt,
+          transactions_1.account_number_from,
+          transactions_1.account_number_to,
+          transactions_1.currency_code,
+          transactions_1.country,
+          transactions_1.status,
+          transactions_1.transaction_type
+SEGMENTED BY hash(transactions_1.account_number_from, transactions_1.account_number_to, transactions_1.currency_code, transactions_1.transaction_dt, transactions_1.country, transactions_1.status, transactions_1.transaction_type, transactions_1.amount) ALL NODES KSAFE 1;
+
+
+SELECT MARK_DESIGN_KSAFE(1);
+
+--currencies
 CREATE TABLE ST23052707__STAGING.currencies
 (
     date_update timestamp,
@@ -35,6 +74,28 @@ CREATE TABLE ST23052707__STAGING.currencies
     currency_with_div numeric(5,3)
 );
 
+CREATE PROJECTION ST23052707__STAGING.currencies /*+createtype(L)*/ 
+(
+ date_update,
+ currency_code,
+ currency_code_with,
+ currency_with_div
+)
+AS
+ SELECT currencies.date_update,
+        currencies.currency_code,
+        currencies.currency_code_with,
+        currencies.currency_with_div
+ FROM ST23052707__STAGING.currencies
+ ORDER BY currencies.date_update,
+          currencies.currency_code,
+          currencies.currency_code_with,
+          currencies.currency_with_div
+SEGMENTED BY hash(currencies.date_update, currencies.currency_code, currencies.currency_code_with, currencies.currency_with_div) ALL NODES KSAFE 1;
+
+
+SELECT MARK_DESIGN_KSAFE(1);
+--transactions_keys
 CREATE TABLE ST23052707__STAGING.transactions_keys
 (
     operation_id varchar(60),
@@ -42,6 +103,25 @@ CREATE TABLE ST23052707__STAGING.transactions_keys
     date_load timestamp
 );
 
+CREATE PROJECTION ST23052707__STAGING.transactions_keys /*+createtype(L)*/ 
+(
+ operation_id,
+ transaction_dt,
+ date_load
+)
+AS
+ SELECT transactions_keys.operation_id,
+        transactions_keys.transaction_dt,
+        transactions_keys.date_load
+ FROM ST23052707__STAGING.transactions_keys
+ ORDER BY transactions_keys.operation_id,
+          transactions_keys.transaction_dt
+SEGMENTED BY hash(transactions_keys.date_load, transactions_keys.transaction_dt, transactions_keys.operation_id) ALL NODES KSAFE 1;
+
+
+SELECT MARK_DESIGN_KSAFE(1);
+
+--global_metrics
 CREATE TABLE ST23052707__DWH.global_metrics
 (
     date_update date,
@@ -51,6 +131,34 @@ CREATE TABLE ST23052707__DWH.global_metrics
     avg_transactions_per_account numeric(10,2),
     cnt_accounts_make_transactions int
 );
+
+CREATE PROJECTION ST23052707__DWH.global_metrics /*+createtype(L)*/ 
+(
+ date_update,
+ currency_from,
+ amount_total,
+ cnt_transactions,
+ avg_transactions_per_account,
+ cnt_accounts_make_transactions
+)
+AS
+ SELECT global_metrics.date_update,
+        global_metrics.currency_from,
+        global_metrics.amount_total,
+        global_metrics.cnt_transactions,
+        global_metrics.avg_transactions_per_account,
+        global_metrics.cnt_accounts_make_transactions
+ FROM ST23052707__DWH.global_metrics
+ ORDER BY global_metrics.date_update,
+          global_metrics.currency_from,
+          global_metrics.amount_total,
+          global_metrics.cnt_transactions,
+          global_metrics.avg_transactions_per_account,
+          global_metrics.cnt_accounts_make_transactions
+SEGMENTED BY hash(global_metrics.date_update, global_metrics.currency_from, global_metrics.amount_total, global_metrics.cnt_transactions, global_metrics.avg_transactions_per_account, global_metrics.cnt_accounts_make_transactions) ALL NODES KSAFE 1;
+
+
+SELECT MARK_DESIGN_KSAFE(1);
 
 -- Запрос для формирования витрины
 INSERT INTO ST23052707__DWH.global_metrics (date_update, currency_from, amount_total, cnt_transactions, avg_transactions_per_account, cnt_accounts_make_transactions)
@@ -73,6 +181,8 @@ FROM (SELECT  DISTINCT date(transaction_dt) as date_update, currency_code as cur
   ) b
 GROUP BY b.date_update, b.currency_from
 
+
+--____________METABASE____________ 	
 -- Примеры создания запросов в METABASE
 --Количества уникальных пользователей
 SELECT DISTINCT
@@ -83,6 +193,10 @@ SELECT DISTINCT
 WHERE a.date_update >= {{start_date}} AND a.date_update <= {{end_date}}
   [[AND currency_from = {{currency_from}}]]
   GROUP BY currency_from,date_update;
+
+
+
+
 
 -- Общий оборот компании в единой валюте
 SELECT DISTINCT
